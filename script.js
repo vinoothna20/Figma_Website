37fb29d1cbd4497399a6c6e656fbd9e56bea917a
vinoothna20 <vinoothnaande20@gmail.com>
2024-07-02 00:03:41 +0530

    figma assignment

---
+++ script.js
@@ -0,0 +1,7 @@
+function displaySignupForm() {
+  document.getElementById("signup").style.display = "block";
+}
+
+function closeSignupForm() {
+  document.getElementById("signup").style.display = "none";
+}
